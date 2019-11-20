3b60f2c918652a13a43ee7c480635fb46d808b07
sehbaoui <mouad>
2019-11-20 17:59:30 +0100

    question 2

---
+++ question2.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'src/mf/utils/ClassLoader.php';
+require_once 'vendor/autoload.php';
+use \mf\utils\ClassLoader as Loader;
+use app\model\Carte as carte;
+
+$loader = new Loader("src");
+$loader->register();
+
+$config = parse_ini_file('conf/config.ini');
+$db = new Illuminate\Database\Capsule\Manager();
+
+$db->addConnection($config);
+$db->setAsGlobal();
+$db->bootEloquent();
+
+$carte_fidel = carte::select("nom_proprietaire","mail_proprietaire","cumul")
+                    ->orderBy("nom_proprietaire", "DESC");
+$uniq = $carte_fidel->get();
+foreach ($uniq as $dede){
+    echo $dede->nom_proprietaire," ",
+    $dede->mail_proprietaire," ",
+    "montant du cumul est"," ",$dede->cumul . PHP_EOL;
+    // here is from dede branch
+}
