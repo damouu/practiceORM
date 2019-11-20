e9af3a83d9136e8908136b1f136c9f36256350a1
sehbaoui <mouad>
2019-11-20 19:08:17 +0100

    question 4

---
+++ question4.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'src/mf/utils/ClassLoader.php';
+require_once 'vendor/autoload.php';
+use \mf\utils\ClassLoader as Loader;
+use app\model\Carte as carte;
+use Illuminate\Database\Eloquent\ModelNotFoundException as ModelNotFoundException;
+
+$loader = new Loader("src");
+$loader->register();
+
+$config = parse_ini_file('conf/config.ini');
+$db = new Illuminate\Database\Capsule\Manager();
+
+$db->addConnection($config);
+$db->setAsGlobal();
+$db->bootEloquent();
+
+try {
+    $carte_fidel = carte::where("nom_proprietaire", "like", "%Ariane%")->firstOrFail();
+    echo $carte_fidel;
+} catch (ModelNotFoundException $e) {
+    echo "no existing card";
+}
+
+// here is from dede branch
