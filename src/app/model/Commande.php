219ce815c10636dbde8d891201be3c3cec54c51c
sehbaoui <mouad>
2019-11-20 21:37:44 +0100

    question 6 , associations 1-n

--- src/app/model/Commande.php
+++ src/app/model/Commande.php
@@ -5,4 +5,8 @@ class Commande extends \Illuminate\Database\Eloquent\Model {
     protected $table = 'commande';
     protected $primaryKey = 'id';
     public $timestamps = false;
+
+    public function infocarte() {
+        return $this->belongsTo('\app\model\Carte', 'carte_id');
+    }
 }
