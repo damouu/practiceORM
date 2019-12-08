ad1e36f612e6f037ac69df6168718f41dc4644ec
sehbaoui <mouad>
2019-12-08 14:20:02 +0100

    question 17 correction

--- src/app/model/Commande.php
+++ src/app/model/Commande.php
@@ -16,6 +16,10 @@ class Commande extends \Illuminate\Database\Eloquent\Model {
         return $this->hasMany('\app\model\item', 'carte_id');
     }
 
+    public function cartes() {
+        return $this->hasMany('\app\model\Carte', 'carte_id');
+    }
+
     public function lesItems() {
         return $this->belongsToMany('\app\model\Item',"item_commande","commande_id","item_id")
             ->withPivot('quantite');
