a608b615fe728fc7e69db270ab27d0ab0046ef4c
damouu <mouadsehbaoui@gmail.com>
2023-01-01 17:52:42 +0900

    refactor re align code

--- src/mf/utils/ClassLoader.php
+++ src/mf/utils/ClassLoader.php
@@ -1,22 +1,27 @@
 <?php
-namespace mf\utils;
 
-class ClassLoader{
+namespace src\mf\utils;
+
+class ClassLoader
+{
 
     private $prefix;
 
-    public function __construct($src){
-        $this->prefix = $src.'/';
+    public function __construct($src)
+    {
+        $this->prefix = $src . '/';
     }
 
-    private function loadClass($str){
-        $class = $this->prefix.str_replace('\\',DIRECTORY_SEPARATOR,$str).'.php';
-        if (file_exists($class)){
+    private function loadClass($str)
+    {
+        $class = $this->prefix . str_replace('\\', DIRECTORY_SEPARATOR, $str) . '.php';
+        if (file_exists($class)) {
             require_once $class;
         }
     }
 
-    public function register(){
+    public function register()
+    {
         spl_autoload_register(array($this, 'loadClass'));
     }
 
