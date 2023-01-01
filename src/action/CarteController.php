d7fb3d8c01a9e4f48772d695edb5e344a9e5748e
damouu <mouadsehbaoui@gmail.com>
2023-01-01 17:50:49 +0900

    Implement and uses the PHP-DI's bridge library for the dependency injection and autowired purposes. and implements the first method getCardsLimit

---
+++ src/action/CarteController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace src\action;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use src\domain\CarteRepository;
+
+class CarteController
+{
+    private $carteRepository;
+
+    public function __construct(CarteRepository $carteRepository)
+    {
+        $this->carteRepository = $carteRepository;
+    }
+
+    public function getCardsLimit(Request $request, Response $response): Response
+    {
+        $queryParams = $request->getQueryParams();
+        $cards = $this->carteRepository->getCardsLimit($queryParams["limit"]);
+        $cardsResponse = [];
+        $cardsCount = 0;
+        foreach ($cards as $card) {
+            $cardsTempArray["nom_proprietaire"] = $card->nom_proprietaire;
+            $cardsTempArray["mail_proprietaire"] = $card->mail_proprietaire;
+            $cardsTempArray["cumul"] = $card->cumul;
+            $cardsResponse[$cardsCount] = $cardsTempArray;
+            $cardsCount++;
+        }
+        $payload = json_encode($cardsResponse, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT);
+        $response->getBody()->write($payload);
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+}
