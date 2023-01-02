12211a219c525945c8badc2e81e1993b68bd327c
damouu <mouadsehbaoui@gmail.com>
2023-01-02 18:03:05 +0900

    refactir adds the findCardOfId function and the __construct for the unit test purposes.

--- src/domain/CarteRepository.php
+++ src/domain/CarteRepository.php
@@ -6,9 +6,24 @@ use src\model\Carte;
 
 class CarteRepository
 {
-    public function getCardsLimit($limitNumber): object
+//    public function __construct(array $cartes = null)
+//    {
+//        $this->cartes = $cartes ?? [
+//            0 => new Carte([0, "dede", "dede", "dede", "dede", '', '', 123])
+//        ];
+//    }
+
+    public function getCardsLimit(int $limitNumber): object
     {
         return carte::select("nom_proprietaire", "mail_proprietaire", "cumul")->limit($limitNumber)->get();
     }
+
+
+    public function findCardOfId(int $id): Carte
+    {
+//        return Carte::findOrFail($id);
+        return $this->cartes[$id];
+    }
+
 
 }
