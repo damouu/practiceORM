584446d3f6ef8b0f0e382f21ad46c47a42222d8a
damouu <mouadsehbaoui@gmail.com>
2023-01-08 01:22:29 +0900

    index.php
    the index.php refactored, so it only got one line of code left, one line to get to the bootstrap.php file who is in charge of setting up and creating the actual app.

--- public/index.php
+++ public/index.php
@@ -1,19 +1,3 @@
 <?php
 
-require '../vendor/autoload.php';
-
-$db = new Illuminate\Database\Capsule\Manager();
-$config = parse_ini_file("../config/config.ini");
-$db->addConnection($config);
-$db->setAsGlobal();
-$db->bootEloquent();
-
-$app = DI\Bridge\Slim\Bridge::create();
-
-$app->get('/api/carte', [src\action\CarteController::class, 'getCardsLimit']);
-
-$app->get('/api/carteid', [src\action\CarteController::class, 'getCartId']);
-
-$app->get('/', [src\action\CarteController::class, 'test']);
-
-$app->run();
+(require __DIR__ . '/../config/bootstrap.php')->run();
