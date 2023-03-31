029ce3f8a5dff6da08e2d4f0c771bdd5aacc6e45
damouu <mouadsehbaoui@gmail.com>
2023-03-31 09:32:14 +0900

    Feature: new route get every command of a given carte

--- config/routes.php
+++ config/routes.php
@@ -6,4 +6,7 @@ return static function (App $app) {
 
     $app->get('/api/carte/{id}[/]', [src\action\CarteController::class, 'getCartId']);
 
+    $app->get('/api/command/{id}[/]', [src\action\CommandeController::class, 'getCommandId']);
+    
+    $app->get('/api/carte/{id}/command', [src\action\CarteController::class, 'getCarteCommand']);
 };
