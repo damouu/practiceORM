563c393eaef1d8c89c95736747e30ff2b114e51e
damouu <mouadsehbaoui@gmail.com>
2023-06-29 15:23:18 +0900

    TD2ORM-21 Done Unassigned Fixed Done
    Feature add function to retrieve command model by given UUID

--- src/domain/CommandeRepository.php
+++ src/domain/CommandeRepository.php
@@ -2,7 +2,14 @@
 
 namespace src\domain;
 
+use src\model\Commande;
+
 class CommandeRepository
 {
+    public function getCommandByUuid(string $uuid): Commande
+    {
+        return Commande::findOrFail($uuid);
+
+    }
 
 }
