16518d9d600554f945c330ad9999146280e85fa4
damouu <mouadsehbaoui@gmail.com>
2023-06-29 15:21:11 +0900

    TD2ORM-21 Done Unassigned Fixed Done
    Refactor pathParam name value to uuid

--- config/routes.php
+++ config/routes.php
@@ -6,7 +6,8 @@ return static function (App $app) {
 
     $app->get('/api/carte/{id}[/]', [src\action\CarteController::class, 'getCartId']);
 
-    $app->get('/api/command/{id}[/]', [src\action\CommandeController::class, 'getCommandId']);
-    
+    $app->get('/api/command/{uuid}[/]', [src\action\CommandeController::class, 'getCommandUuid']);
+
     $app->get('/api/carte/{id}/command', [src\action\CarteController::class, 'getCarteCommand']);
+
 };
